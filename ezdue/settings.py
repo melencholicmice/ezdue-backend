201ce24e39c160cdc53de643bee32049224d5196
melencholicmice <109169835+melencholicmice@users.noreply.github.com>
2024-03-19 00:04:26 +0530

    cors fixes

--- ezdue/settings.py
+++ ezdue/settings.py
@@ -25,9 +25,14 @@ SECRET_KEY = 'django-insecure-*_bplcfbkd=c168(6^k3a(z^@y*$wo+-m-$&1jo=fzcqkj8(0c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "*"
+ALLOWED_HOSTS = []
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 
 # Application definition
@@ -49,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
